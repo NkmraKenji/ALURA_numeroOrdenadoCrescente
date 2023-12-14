24ce5bddd69a5a21f181ae1f02a7b91ddcec234d
Patrick Kenji Nakamura <nakapatken@gmail.com>
2023-12-14 18:35:42 +0900

    First commit

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let num1 = parseFloat(prompt('Digite o primeiro número: '));
 let num2 = parseFloat(prompt('Digite o primeiro número: '));
 let num3 = parseFloat(prompt('Digite o primeiro número: '));
-
-function ordemCrescente (num1, num2, num3) {
+let num4 = parseFloat(prompt('Digite o primeiro número: '));
+function ordemCrescente (num1, num2, num3, num4) {
     let num_ordenado = [num1, num2, num3].sort((x, y) => x-y);
     console.log(`Números ordenados: ${num_ordenado.join(', ')}`)
 }
 
-ordemCrescente(num1, num2, num3);
+ordemCrescente(num1, num2, num3, num4);
